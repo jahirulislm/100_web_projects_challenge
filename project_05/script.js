3427418b266986cb06730335ec3beacf1fabbf47
Jahirul Islam <86736791+jahirulislm@users.noreply.github.com>
2023-09-25 16:28:16 +0600

    Add files via upload

---
+++ project_05/script.js
@@ -0,0 +1,35 @@
+const mobileMenu = document.querySelector(".mobile-menu");
+const navMenu = document.querySelector(".menu");
+
+mobileMenu.addEventListener("click", () => {
+  navMenu.classList.toggle("mobileScreen-menu");
+
+  // humbergur menu toggle
+  mobileMenu.classList.toggle("toggle");
+});
+
+// Video pop up
+
+window.document.onkeydown = function (e) {
+  if (!e) {
+    e = event;
+  }
+  if (e.keyCode == 27) {
+    lightbox_close();
+  }
+};
+
+function lightbox_open() {
+  var lightBoxVideo = document.getElementById("VisaChipCardVideo");
+  window.scrollTo(0, 0);
+  document.getElementById("light").style.display = "block";
+  document.getElementById("fade").style.display = "block";
+  lightBoxVideo.play();
+}
+
+function lightbox_close() {
+  var lightBoxVideo = document.getElementById("VisaChipCardVideo");
+  document.getElementById("light").style.display = "none";
+  document.getElementById("fade").style.display = "none";
+  lightBoxVideo.pause();
+}
